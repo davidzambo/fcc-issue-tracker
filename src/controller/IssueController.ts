b154cd8fcd60c6e2e2aa7eb852be3d38ab844ce6
David Zambo <david@dcmf.hu>
2018-07-29 01:27:15 +0200

    Working on update

--- src/controller/IssueController.ts
+++ src/controller/IssueController.ts
@@ -14,16 +14,28 @@ export class IssueController {
     }
 
     public static create(req: express.Request, res: express.Response) {
+        /*
+        * Find the given project
+        * */
         Project.findOne({project_name: req.params.projectname},
             (err: any, project: any) => {
             if (err) return console.error(err);
+
+            /*
+            * Handle "no project" case
+            * */
             if (!project) return res.json({
                 message: "Unknown project!"
             });
+
+
             project.issues.push(req.body);
             project.save((error: any) => {
                 if (error) {
                     let errors: any = [];
+                    /*
+                    * Fetch all error messages and return it to the user
+                    */
                     Object.keys(error.errors).forEach((key) => {
                         errors.push(error.errors[key].message);
                     });
@@ -32,7 +44,7 @@ export class IssueController {
                 /*
                 * Send back the last inserted issue
                 */
-                return res.json(project.issues[project.issues.length - 1]);
+                return res.render("index", {result: project.issues[project.issues.length - 1]});
             });
         });
     }
@@ -44,6 +56,56 @@ export class IssueController {
     }
 
     public static update(req: express.Request, res: express.Response) {
+        try {
+            /*
+            * Check validation of given _id
+            * */
+            const _id = new mongoose.Types.ObjectId(req.body._id);
+            /*
+            * Check new data if they have any values
+            * */
+            let hasSomethingToUpdate = false;
+            for (let key in req.body) {
+                if (key !== "_id" && req.body[key] !== "") {
+                    hasSomethingToUpdate = true;
+                    break;
+                }
+            }
+
+            if (!hasSomethingToUpdate) {
+                return res.json({
+                    message: "No updated field sent!"
+                })
+            }
+            /*
+            * Find the given project
+            * */
+            Project.findOneAndUpdate({
+                    project_name: req.params.projectname,
+                    "issues._id": _id
+                },
+                {"$set": {
+                        "issues.$": req.body
+                    }}, (err: any, project: any) => {
+                    if (err) return res.json({
+                        message: "Something went wrong!",
+                        error: err
+                    });
+
+                    /*
+                    * Handle "no project" case
+                    * */
+                    if (!project) return res.json({
+                        message: "Unknown project!"
+                    });
+
+                });
+        } catch (error) {
+            return res.json({
+                message: "Whoops! Something went wrong!",
+                error: error.message
+            })
+        }
 
     }
 
