06eada026b13ae0e15bdd670e8f0d404841a9819
David Zambo <david@dcmf.hu>
2018-08-28 08:33:34 +0200

    Main page frontend looks and behaves like expected

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-    $('#description').on('mouseover', function(){
-        $('#content').animate({
-            'margin-top': 0
-        }, 200, function(){
+    $('#description').on('click', function(){
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $('body').css('height', 'min-content');
             $('#project-description').animate({
-                opacity: 1
-            }, 200);
-        });
+                height: '100%',
+                padding: "2rem 1rem"
+            }, 200, function() {
+                $(this).animate({
+                    opacity: 1,
+                },100);
+            });
+        } else {
+            $('body').css('height', window.innerWidth < 768 ? 'min-content' : '100%');
+            $('#project-description').animate({
+                opacity: 0,
+            }, 200, function(){
+                $(this).animate({
+                    padding: 0,
+                    height: 0
+                }, 200);
+            });
+        }
     });
 });
