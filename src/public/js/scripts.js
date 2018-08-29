6b7952c3c513238bc9ce71c5e725265c7f5d4e2d
David Zambo <david@dcmf.hu>
2018-08-29 09:37:05 +0200

    Form handling with ajax

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
+function IssueTracker(){
+    const self = this;
+    self.handleDescription();
+    self.handleForm();
+}
+
+IssueTracker.prototype.handleDescription = function(){
     $('#description').on('click', function(){
         $(this).toggleClass('active');
         if ($(this).hasClass('active')) {
@@ -21,4 +27,44 @@ $(document).ready(function(){
             });
         }
     });
+};
+
+IssueTracker.prototype.handleForm = function () {
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        $('#response').empty();
+        const formData = {};
+        const method = $(this).attr('method');
+        $('form[method= ' + post + ']')
+            .find('input, select, textarea')
+            .map(function(){
+                formData[this.name] =  this.value;
+        });
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: formData,
+            cache: false,
+            processData: true,
+            contentType: false,
+            success: function(response){
+                $('#response').text(JSON.stringify(response));
+                // response.error.map(el => {
+                //     $('#response').append($('<p>').text(el));
+                // });
+                $('#result').show();
+            },
+            error: function(response) {
+                console.log(response);
+            }
+        });
+    });
+};
+
+$(document).ready(function(){
+    window.issueTracker = new IssueTracker();
+
+
+
 });
